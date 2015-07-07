389732ac7960f677a6d38a9f7cebad22ff769a1b
JamesGotsell <j.e.gotsell@gmail.com>
2015-07-07 15:23:56 +0100

    working on the core.js

--- core.js
+++ core.js
@@ -1,1 +1,16 @@
 var module = angular.module("sampleApp", ['ngRoute']);
+
+module.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+  when('/route1/:param', {
+    templateUrl: 'angular-route-template-1.jsp',
+    controller: 'RouteController'
+  }).
+  when('/route2/:param',{
+    templateUrl: 'angular-route-template-2.jsp',
+    controller: 'RouteController'
+  }).
+  otherwise ({
+      redirectTo: '/'
+  });
+}]);
